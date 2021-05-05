c64194d06a189f74fa8273ecab41e04ee040a485
Perr0fe0 <marco.aquile74@gmail.com>
2021-05-04 23:50:34 -0400

    Conexion agregada junto con la libreria

---
+++ src/Conexion/ProbarConexion.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Conexion;
+
+/**
+ *
+ * @author totit
+ */
+public class ProbarConexion {
+    
+    public static void main(String [] arg){
+        Connect co= new Connect();
+        co.getConnection();
+    }
+    
+
+
+}
